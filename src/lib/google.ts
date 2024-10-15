2d2c4eaa047678ce147f1950cb2d5ef4497713ef
Pushpank Dhruw <Pushpank1010@Gmail.Com>
2024-10-15 19:59:55 +0530

    🛠️ fix google api env

--- src/lib/google.ts
+++ src/lib/google.ts
@@ -1,7 +1,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const ai = new GoogleGenerativeAI(
-  process.env.NEXT_PUBLIC_GOOGLE_API_KEY as string
+  process.env.GOOGLE_API_KEY as string
 );
 
 export default ai;
